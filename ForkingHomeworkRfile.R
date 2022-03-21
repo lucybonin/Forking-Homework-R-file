40acffeb17ddc0faa764f957ff5f233100ea8f29
Lucy <lucy.bonin@gmail.com>
2022-03-21 14:17:30 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,8 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+#convert to data.table
+diamonds1<-diamonds
+setDT(diamonds1)
+diamonds1[cut == "Premium" & price > 7000 & price <10000,.(carat,cut,price)][order(-carat)][c(1:20)]
